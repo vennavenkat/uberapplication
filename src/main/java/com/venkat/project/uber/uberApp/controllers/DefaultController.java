0f6868bac515641040144e821810127c01bd8abc
vennavenkat <vennavenkat1999@gmail.com.com>
2024-10-17 21:12:15 -0500

    security changes

--- src/main/java/com/venkat/project/uber/uberApp/controllers/DefaultController.java
+++ src/main/java/com/venkat/project/uber/uberApp/controllers/DefaultController.java
@@ -1,14 +1,12 @@
 package com.venkat.project.uber.uberApp.controllers;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.view.RedirectView;
 
-@Controller
 public class DefaultController {
+	 @GetMapping("/")
+	    public RedirectView redirectToSwaggerUi() {
+	        return new RedirectView("/swagger-ui/index.html");
+	    }
 
-    @GetMapping("/")
-    public RedirectView redirectToGoogle() {
-        return new RedirectView("https://www.google.com");
-    }
 }
